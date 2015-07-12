e1b3af2f95c011dc57eefae7e92d8fd65cb48c7b
tux-mind <massimo.dragano@gmail.com>
2015-07-12 19:06:57 +0200

    created a script to automatically generate content

---
+++ export.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+test "$#" -eq 1 || { echo "Usage: $0 url"; exit 1 ;}
+
+wget -m -nH $1
+
+# use relative paths
+sed -E -i s,/assets,assets,g index.html
+
+# remove csrf tokens
+sed -i -E '/^<meta name="csrf/d' index.html
